94f3a299e24e5a76b7fb5fc9112bac35ae944f03
Dylan <jvo78010@gmail.com>
2025-12-08 21:58:16 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById("navbar").innerHTML = `
+
+    <nav> 
+        <h3> <a href="index.html"> <span>[Home]</span> | <a> <a href="svj.html"> <span>[SVJ]</span> <a>  | <a> <a href="senna.html"> <span>[Senna]</span> </h3>
+    </nav>
+`; 
+
